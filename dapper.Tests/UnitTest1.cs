6fc56a2e98b0345b48a5ebc32b016f2c7b0279d5
romulofgouvea <romulo-gouvea@hotmail.com>
2021-02-09 08:23:16 -0300

    Criando a estrutura de projeto

---
+++ dapper.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dapper.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
